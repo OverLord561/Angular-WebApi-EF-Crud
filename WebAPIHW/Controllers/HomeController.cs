d7d80ccad415a56bc2318d87860405f283736e1d
overlord561 <yurapuk452@gmail.com>
2016-11-17 01:47:00 +0200

    first demo

---
+++ WebAPIHW/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAPIHW.Models;
+
+namespace WebAPIHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            var books = db.Books.ToList();
+            return View();
+        }
+
+        public ActionResult Add()
+        {    
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
